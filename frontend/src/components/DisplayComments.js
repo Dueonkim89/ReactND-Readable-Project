ed5dc871cd114399ca9016ef7182c1fbead00069
Dueon Kim <dueonkim89@yahoo.com>
2018-01-17 19:34:47 -0800

    Responsive layout created for the comments mapped from store.

--- frontend/src/components/DisplayComments.js
+++ frontend/src/components/DisplayComments.js
@@ -2,3 +2,6 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import downArrowIcon from '../icons/downArrowIcon.svg';
 import upArrowIcon from '../icons/upArrowIcon.svg';
+
+
+//comments.fiter( x => x.parentId === postInfo).map( eachComment );
--- frontend/src/components/RouteThePosts.js
+++ frontend/src/components/RouteThePosts.js
@@ -73,7 +73,7 @@ class RouteThePosts extends Component {
 						</Row>
 					</Jumbotron>
 					{postInfo !== null && posts.filter(post => post.id === postInfo).map( eachPost => (
-						<div key={eachPost.id}>
+						<div key={eachPost.id} className='postDiv'>
 							<Row className="routePostContainer">
 								<Col xs={1} md={1} className='iconDiv'>
 									<a className="topArrow" onClick={ ()=> this.vote(eachPost.id, 'upVote') }>	
@@ -101,16 +101,63 @@ class RouteThePosts extends Component {
 								<Col xs={11} xsOffset={1}>
 									<p className="postBody">{eachPost.body}</p>									
 								</Col>
-							</Row>
-							<Row className="commentButtonContainer">
-								<Col xs={5} xsOffset={1}>
-									<span className='comments'>{numberOfComments} Comments </span>
-								</Col>
-							</Row>
-							{/* Pass in parent id of post, so comments can be created.*/}
+							</Row>							
 						</div>
 					))}
-					
+						{/* Filter comments by parentId to get relevant comments, Then map  DisplayComments component*/}
+						<Row className="commentButtonContainer">
+							<Col xs={5} xsOffset={1} sm={2} md={2}>
+								<span className='comments'>{numberOfComments} Comments </span>									
+							</Col>
+							<Col xs={5} sm={2} md={3}>
+								<Button onClick={ () => console.log('created option to add comment')} 
+												style={{padding:'0 1.25rem'}} 
+												className='commentButton'
+												bsStyle="link">
+												Add Comment
+								</Button>								
+							</Col>
+						</Row>							
+						{comments.filter( x => x.parentId === postInfo).map( eachComment => (
+							<div key={eachComment.id} className='commentDiv'>
+								<Row className="commentContainer">
+									<Col xs={1} md={1} className='iconDiv'>
+										<a className="topArrow" onClick={ ()=> console.log('create method for comment arrow') }>	
+											<img src={upArrowIcon} alt='Up Arrow Icon'/>
+										</a> 
+										<a className="downArrow" onClick={ ()=> console.log('create method for comment arrow') }>																
+											<img src={downArrowIcon} alt='Down Arrow Icon'/>
+										</a>
+										{ eachComment.voteScore < 0 ? (
+											<span className="voteScoreNegative">{eachComment.voteScore}</span>	
+										) : (
+											<span className="voteScore">{eachComment.voteScore}</span>	
+										)}									
+									</Col>
+									<Col xs={11} md={9} className='titleDiv'>
+										<h1 className='commentTitle'>{eachComment.body}</h1>
+										<div className='categoryDiv'>
+											<span className='author'>Author: {eachComment.author}</span>
+											<span className='date'>Date: {this.getDate(eachComment.timestamp)}</span>
+											<Button onClick={ () => console.log('create modal to edit comment') } 
+													className='editCommentButton'
+													bsStyle="primary">
+													Edit
+											</Button>									
+											<Button onClick={ () => console.log('create action and reducer to delete comment') } 
+													className='deleteCommentButton'
+													bsStyle="primary">
+													Delete
+											</Button>											
+										</div>																
+									</Col>										
+								</Row>					
+							
+							
+							</div>
+						))}
+						
+						
 				</div>		
 		);
 	} 
