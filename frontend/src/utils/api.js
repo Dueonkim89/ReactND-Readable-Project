81bc8b1f7f70d0a9234b38647df0005a6de245f8
Dueon Kim <dueonkim89@Yahoo.com>
2018-07-10 00:13:39 -0700

    restructure server-side files and directory

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,10 @@
 const api = 'http://localhost:3001';
 let token = localStorage.token;
 
-if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
+if (!token) {
+	token = localStorage.token = Math.random().toString(36).substr(-8);
+}
+  
 
 const headers = {
 	'Accept': 'application/json',
